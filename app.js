c3aeb371bec99f35ab3f42279743e426dff52e17
Mathias Roux <mathiasroux.pro@gmail.com>
2022-11-15 18:47:18 +0100

    start home page

--- app.js
+++ app.js
@@ -14,7 +14,7 @@ const prismic = require('@prismicio/client')
 const fetch = (...args) => import('node-fetch').then(({ default: fetch }) => fetch(...args));
 const prismicH =  require('@prismicio/helpers')
 
-const endpoint = prismic.getEndpoint("floema-rouxmathias");
+const endpoint = prismic.getEndpoint("personal-portfoliov2");
 const client = prismic.createClient(endpoint, { fetch });
 
 
@@ -26,21 +26,23 @@ app.use(errorHandler())
 app.use(express.static(path.join(__dirname, 'public')))
 
 const handleLinkResolver = (doc) => {
-  if (doc.type === 'product') {
-    return `/detail/${doc.slug}`
+  // if (doc.type === 'project') {
+  //   return `/detail/${doc.slug}`
+  // }
+
+  if (doc.type === 'home') {
+    return '/home'
   }
-  if (doc.type === 'collections') {
-    return '/collections'
-  }
-  if (doc.type === 'about') {
-    return '/about'
-  }
+  // if (doc.type === 'about') {
+  //   return '/about'
+  // }
 
   return null
 }
 
 const prismicAutoPreviewsMiddleware = (req, _res, next) => {
   client.enableAutoPreviewsFromReq(req)
+
   next()
 }
 
@@ -48,9 +50,6 @@ app.use((req, res, next) => {
   res.locals.ctx = { prismicH }
   prismicAutoPreviewsMiddleware
   res.locals.Link = handleLinkResolver
-  res.locals.Numbers = index => {
-    return index == 0 ? 'One' : index == 1 ? 'Two' : index == 2 ? 'Three' : index == 3 ? 'Four' : ''
-  }
 
   next()
 })
@@ -61,46 +60,46 @@ app.set('view engine', 'pug')
 
 const handleRequest = async () => {
   const meta = await client.getSingle('meta')
-  let assets = []
+  // let assets = []
   const navigation = await client.getSingle('navigation')
   const preloader = await client.getSingle('preloader')
-  const home = await client.getSingle('home')
-  const about = await client.getSingle('about')
-  const collections = await client.getAllByType('collection', {
+  // const about = await client.getSingle('about')
+  const home = await client.getAllByType('item', {
     fetchLinks: 'product.image'
   })
 
-  home.data.gallery.forEach(item => {
-    assets.push(item.image.url)
-  })
+  // console.log(home[0])
 
-  about.data.gallery.forEach(item => {
-    assets.push(item.image.url)
-  })
+  // home.forEach(item => {
+  //   console.log(item.data.image)
+  // })
 
-  about.data.body.forEach(section => {
-    if (section.slice_type === 'gallery') {
-      section.items.forEach(item => {
-        assets.push(item.image.url)
-      })
-    }
-  })
+  // about.data.gallery.forEach(item => {
+  //   assets.push(item.image.url)
+  // })
 
-  collections.forEach(collection => {
-    collection.data.products.forEach(item => {
-      assets.push(item.products_product.data.image.url)
-    })
-  })
+  // about.data.body.forEach(section => {
+  //   if (section.slice_type === 'gallery') {
+  //     section.items.forEach(item => {
+  //       assets.push(item.image.url)
+  //     })
+  //   }
+  // })
+
+  // home.forEach(home => {
+  //   home.data.products.forEach(item => {
+  //     assets.push(item.products_product.data.image.url)
+  //   })
+  // })
 
 
   return {
     meta,
-    assets,
+    // assets,
     navigation,
     preloader,
     home,
-    about,
-    collections
+    // about
   }
 }
 
@@ -112,34 +111,26 @@ app.get('/', async (req, res) => {
   })
 })
 
-app.get('/about', async (req, res) => {
-  const defaults = await handleRequest()
+// app.get('/detail/:uid', async (req, res) => {
+//   const defaults = await handleRequest()
 
-  res.render('pages/about', {
-    ...defaults
-  })
-})
+//   const product = await client.getByUID('product', req.params.uid, {
+//     fetchLinks: 'collection.title'
+//   })
 
-app.get('/collections', async (req, res) => {
-  const defaults = await handleRequest()
+//   res.render('pages/detail', {
+//     ...defaults,
+//     product
+//   })
+// })
 
-  res.render('pages/collections', {
-    ...defaults
-  })
-})
+// app.get('/about', async (req, res) => {
+//   const defaults = await handleRequest()
 
-app.get('/detail/:uid', async (req, res) => {
-  const defaults = await handleRequest()
-
-  const product = await client.getByUID('product', req.params.uid, {
-    fetchLinks: 'collection.title'
-  })
-
-  res.render('pages/detail', {
-    ...defaults,
-    product
-  })
-})
+//   res.render('pages/about', {
+//     ...defaults
+//   })
+// })
 
 
 app.listen(port, () => {
