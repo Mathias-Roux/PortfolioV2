e9145f6cfd1677ce882acfc796141e839bc7b3ef
Mathias-Roux <mathiasroux.pro@gmail.com>
2023-01-14 21:15:41 +0100

    build

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ const methodOverride = require('method-override')
 
 const app = express()
 const path = require('path')
-const port = 8080
+const port = 3000
 
 const prismic = require('@prismicio/client')
 const fetch = (...args) => import('node-fetch').then(({ default: fetch }) => fetch(...args));
