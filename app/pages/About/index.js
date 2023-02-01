fe508bfd641c07e13638891302d4feec054ae6e5
Mathias-Roux <mathiasroux.pro@gmail.com>
2023-02-01 13:57:05 +0100

    about text animation

--- app/pages/About/index.js
+++ app/pages/About/index.js
@@ -1,4 +1,5 @@
-import GSAP, { splitColor } from 'gsap'
+import GSAP from 'gsap'
+import { split } from 'utils/text'
 import each from 'lodash/each'
 
 import Page from "../../classes/Page"
@@ -22,25 +23,31 @@ export default class About extends Page {
   textAnimation(){
     this.page = document.querySelector('.about')
 
-    this.text = about__infos__row__text
+    this.texts = this.page.querySelectorAll('h1, h2, p')
 
-    split({
-      element: this.text,
-      expression: '<br>'
+    each(this.texts, text => {
+      split({
+        element: text,
+        expression: '<br>'
+      })
+      split({
+        element: text,
+        expression: '<br>'
+      })
     })
-    split({
-      element: this.text,
-      expression: '<br>'
-    })
+    
+    this.spans = this.page.querySelectorAll('span span')
   
 
     this.animationIn = GSAP.timeline()
-    this.animationIn.set(this.texts,{
-      y: '100%'
+    this.animationIn.set(this.spans,{
+      y: '100%',
     }) 
 
-    this.animationIn.to(this.texts,{
-      y: '0%'
+    this.animationIn.to(this.spans,{
+      y: '0%',
+      ease: 'cubic-bezier(0.77, 0, 0.175, 1)',
+      duration: .5
     }, '+=1')
   }
 
