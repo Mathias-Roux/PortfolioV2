2613a71617eef046584d43531a5d4725f73936b7
Mathias-Roux <mathiasroux.pro@gmail.com>
2023-05-19 17:12:40 +0200

    alt text anim

--- app/shaders/plane-vertex.glsl
+++ app/shaders/plane-vertex.glsl
@@ -4,8 +4,6 @@ attribute vec2 uv;
 uniform mat4 modelViewMatrix;
 uniform mat4 projectionMatrix;
 
-uniform sampler2D texture;
-
 uniform float time;
 uniform float amplitude;
 uniform float frequency;
@@ -19,13 +17,12 @@ varying vec2 vDistortedUv;
 void main() {
     vUv = uv;
 
-    vec4 offset = texture2D(texture, vUv);
     vec3 displacedPosition = position;
 
     float waveX = position.x + time * waveSpeed * frequency;
     float waveY = position.y + time * waveSpeed * frequency;
 
-    displacedPosition.z += sin(waveX + waveY) * amplitude * waveHeight * offset.r;
+    displacedPosition.z += sin(waveX + waveY) * amplitude * waveHeight;
     
     vDistortedUv = vec2(
         vUv.x + sin(waveX + waveY) * amplitude * 0.0,
